765cab34f07b9b5fb1ae698f20413c997a41ebf5
James Benjamin <james.abenj@gmail.com>
2019-11-18 21:32:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, "male", "brown", "full", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "male", "red", "hyper", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, "female", "yellow", "quiet", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 20, "male", "brown", "serious", FALSE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, "female", "black", "chatty", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 17, "male", "green", "crabby", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, "female", "white", "tired", TRUE);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "male", brown, "aggresive", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, "male", "brown", "full", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "male", "red", "hyper", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, "female", "yellow", "quiet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 20, "male", "brown", "serious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, "female", "black", "chatty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 17, "male", "green", "crabby", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, "female", "white", "tired", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "male", brown, "aggresive", 0);
